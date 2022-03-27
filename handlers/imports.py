30a16b727b685811c84b8b4c84e15b5a70ab43f7
yusupovanton <antonyusupov@outlook.com>
2022-03-28 00:17:49 +0300

    changed back to server instad of :app

--- handlers/imports.py
+++ handlers/imports.py
@@ -1,10 +1,11 @@
 import dash
 from dash import Dash, dcc, html, Input, Output, dash_table
 from dash.dependencies import Output, Input
-from flask import Flask
 import plotly.express as px
 from plotly.express import data
 import statsmodels
 import gunicorn
 import redis
 import pandas as pd
+
+from flask import Flask, Blueprint
